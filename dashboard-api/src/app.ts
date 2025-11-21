8f9fa5a17b668ef4e68f1bec77cca30630e45b82
Dmitriy <122107908+NxRDeagle@users.noreply.github.com>
2025-11-22 04:10:51 +0500

    switch code generation mode to commonJS

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,38 +1,44 @@
-import express from "express";
-import { Server } from "http";
-import { inject, injectable } from "inversify";
-import { UserController } from "./users/users.controller";
-import { ExceptionFilter } from "./errors/exception.filter";
-import { ILogger } from "./logger/logger.interface";
-import { TYPES } from "./types";
+import express from 'express';
+import { Server } from 'http';
+import { inject, injectable } from 'inversify';
+import { json } from 'body-parser';
+import { UserController } from './users/users.controller';
+import { ExceptionFilter } from './errors/exception.filter';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
 
 @injectable()
 export class App {
-  app: express.Express;
-  port: number;
-  server: Server;
+	app: express.Express;
+	port: number;
+	server: Server;
 
-  constructor(
-    @inject(TYPES.ILogger) private logger: ILogger,
-    @inject(TYPES.UserController) private userController: UserController,
-    @inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter
-  ) {
-    this.app = express();
-    this.port = 8000;
-  }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-  useRoutes() {
-    this.app.use("/users", this.userController.router);
-  }
+	useRoutes() {
+		this.app.use('/users', this.userController.router);
+	}
 
-  useExceptionFilters() {
-    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
-  }
+	useExceptionFilters() {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
 
-  public async init() {
-    this.useRoutes();
-    this.useExceptionFilters();
-    this.server = this.app.listen(this.port);
-    this.logger.log(`Server started at http://localhost:${this.port}`);
-  }
+	useMiddleware() {
+		this.app.use(json());
+	}
+
+	public async init() {
+		this.useMiddleware();
+		this.useRoutes();
+		this.useExceptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Server started at http://localhost:${this.port}`);
+	}
 }
--- dashboard-api/src/users/users.controller.ts
+++ dashboard-api/src/users/users.controller.ts
@@ -1,24 +1,26 @@
-import { injectable, inject } from "inversify";
-import { Request, Response, NextFunction } from "express";
-import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
-import { TYPES } from "../types";
-import { IUserController } from "./users.controller.interface";
+import { injectable, inject } from 'inversify';
+import { Request, Response, NextFunction } from 'express';
+import { BaseController } from '../common/base.controller';
+import { LoggerService } from '../logger/logger.service';
+import { TYPES } from '../types';
+import { IUserController } from './users.controller.interface';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-  constructor(@inject(TYPES.ILogger) LoggerService: LoggerService) {
-    super(LoggerService);
-    this.bindRoutes([
-      { path: "/register", method: "post", func: this.register },
-      { path: "/login", method: "post", func: this.login },
-    ]);
-  }
+	constructor(@inject(TYPES.ILogger) LoggerService: LoggerService) {
+		super(LoggerService);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
 
-  register(request: Request, response: Response, next: NextFunction) {
-    this.ok(response, "register");
-  }
-  login(request: Request, response: Response, next: NextFunction) {
-    this.ok(response, "login");
-  }
+	register(request: Request<{}, {}, UserRegisterDto>, response: Response, next: NextFunction) {
+		this.ok(response, 'register');
+	}
+	login(request: Request<{}, {}, UserLoginDto>, response: Response, next: NextFunction) {
+		this.ok(response, 'login');
+	}
 }
